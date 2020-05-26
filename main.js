04d029d74a0e1b7d977fe142455baeee70014af2
GoneAway <vasil.zisis@gmail.com>
2020-05-26 21:38:54 +0300

    no message

--- main.js
+++ main.js
@@ -3,19 +3,30 @@
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var i;
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
+var img = document.getElementsByClassName("modalIMG");
+var modalImg = document.getElementById("showIMG");
+
+for (i = 0; i < img.length; i++) {
+    img[i].onclick = function () {
+
+        modal.style.display = "block";
+        modalImg.src = this.src;
+
+    }
 }
-                                         
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
