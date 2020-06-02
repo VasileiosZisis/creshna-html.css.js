c816a3cb9f28809cc0f8957878d93f913b6b0c62
GoneAway <vasil.zisis@gmail.com>
2020-06-02 20:39:24 +0300

    Changed resolution of photos when clicked Added correct redirect url to bandcamp for merch

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ span.onclick = function () {
     modal.style.display = "none";
     document.body.style.overflow = "visible";
 }
-
+// Close the modal when user clicks outside the image
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
