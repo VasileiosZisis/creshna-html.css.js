b42102cd013ac4161c54b7ed7374f336cbd0778d
GoneAway <vasil.zisis@gmail.com>
2021-03-29 21:45:33 +0300

    VERSION 2

--- main.js
+++ main.js
@@ -1,35 +1,5 @@
-/*eslint-env browser*/
-/*jslint browser: true*/
+const header = document.querySelector('header');
 
-
-
-var modal = document.getElementById("myModal");
-var i;
-
-var img = document.getElementsByClassName("modalIMG");
-var modalImg = document.getElementById("showIMG");
-
-for (i = 0; i < img.length; i++) {
-    img[i].onclick = function () {
-
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        document.body.style.overflow = "hidden";
-    }
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-    document.body.style.overflow = "visible";
-}
-// Close the modal when user clicks outside the image
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        document.body.style.overflow = "visible";
-    }
-}
+window.addEventListener('scroll', () => {
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
