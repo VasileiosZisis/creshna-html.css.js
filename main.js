2bc0cf40c58a43fc878baa10d0b1f37eb5a9a142
GoneAway <vasil.zisis@gmail.com>
2021-04-02 19:55:16 +0300

    no message

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const header = document.querySelector('header');
+const text = document.querySelectorAll('.text');
+
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+observerText = new IntersectionObserver((entriesText) => {
+    entriesText.forEach(entryText => {
+        if(entryText.isIntersecting === true) {
+        entryText.target.classList.add('textAnimation');
+        }
+        else
+        entryText.target.classList.remove('textAnimation');
+    })
+})
+  
+text.forEach(pathText => {
+    observerText.observe(pathText);
+})
