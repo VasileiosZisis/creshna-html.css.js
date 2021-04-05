f73728b21bfef459bf7f30bbcea273d7eb67b174
GoneAway <vasil.zisis@gmail.com>
2021-04-05 22:19:27 +0300

    no message

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 const header = document.querySelector('header');
-const text = document.querySelectorAll('.observer');
+const animation = document.querySelectorAll('.animation');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-observerText = new IntersectionObserver((entriesText) => {
-    entriesText.forEach(entryText => {
-        if(entryText.isIntersecting === true) {
-        entryText.target.classList.add('textAnimation');
+// ANIMATION
+observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0) {
+        entry.target.classList.add('animate');
         }
-        else
-        entryText.target.classList.remove('textAnimation');
     })
 })
   
-text.forEach(pathText => {
-    observerText.observe(pathText);
+animation.forEach(path => {
+    observer.observe(path);
 })
+
+// MODAL BOX
+
+
+// BACK TO TOP
+$(document).ready(function($){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $(mybutton).fadeIn('slow');
+        } else {
+            $(mybutton).fadeOut('slow');
+        }
+    });
+    $(mybutton).click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
+  });
+  
+  //SMOOTH SCROLL
+  $(document).ready(function() {
+      $("a").on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+  
+          var hash = this.hash;
+  
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1000, function(){
+            window.location.hash = hash;
+        });
+      }
+    });
+  });
