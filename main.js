3565806e3abaecd6039b3c52c0b25e4c58545808
GoneAway <vasil.zisis@gmail.com>
2021-04-07 22:21:48 +0300

    no message

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const header = document.querySelector('header');
 const animation = document.querySelectorAll('.animation');
+const modalEle = document.querySelector('.modal');
+const modalImage = document.querySelector('.modal-content');
+const close = document.querySelector('.close');
+const modalIMG = document.querySelectorAll('.modalIMG')
+
+window.ondragstart = () => {
+     return false; 
+    }
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
@@ -19,7 +27,46 @@ animation.forEach(path => {
 })
 
 // MODAL BOX
+modalIMG.forEach(item => {
+   item.addEventListener("click", event => {
+      modalEle.style.display = "block";
+      modalImage.src = event.target.src;
+      document.body.style.overflow = "hidden";
+   });
+});
 
+close.addEventListener("click", () => {
+   modalEle.style.display = "none";
+   document.body.style.overflow = "visible";
+});
+
+// SLIDER
+const slider = document.querySelector('.photosGrid');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
 
 // BACK TO TOP
 $(document).ready(function($){
