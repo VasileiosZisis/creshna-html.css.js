4932a78bfb646db95d6efe6194226bab6c65f562
GoneAway <vasil.zisis@gmail.com>
2022-10-05 21:24:45 +0300

    .

--- main.js
+++ main.js
@@ -3,63 +3,83 @@ const modalImage = document.querySelector('.modal-content');
 const close = document.querySelector('.close');
 const modalIMG = document.querySelectorAll('.modal-IMG');
 
+const lines = document.querySelectorAll('hr');
+
+// OBSERVER
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+
+lines.forEach((line) => {
+  observer.observe(line);
+});
+
 // TRANSITION
 const transitionEl = document.querySelector('.transition');
 
 window.onload = () => {
   setTimeout(() => {
-transitionEl.classList.remove('is-active');
-  },500)
-}
+    transitionEl.classList.remove('is-active');
+  }, 500);
+};
 
 // SPLIDE
-var elms = document.getElementsByClassName( 'splide' );
-for ( var i = 0, len = elms.length; i < len; i++ ) {
-	new Splide( elms[ i ], {
+var elms = document.getElementsByClassName('splide');
+for (var i = 0, len = elms.length; i < len; i++) {
+  new Splide(elms[i], {
+    padding: {
+      left: '4rem',
+      right: 0,
+    },
+    type: 'slide',
+    perPage: 4,
+    perMove: 1,
+    height: '290px',
+    breakpoints: {
+      1530: {
+        perPage: 3,
+      },
+      950: {
+        perPage: 2,
+      },
+      680: {
+        perPage: 1,
+      },
+      500: {
+        perPage: 1,
         padding: {
-            left : '4rem',
-            right:0,
+          left: '1rem',
+          right: 0,
         },
-        type      : 'slide',
-        perPage   : 4,
-        perMove   : 1,
-        height: '290px',
-        breakpoints: {
-          1530: {
-            perPage: 3,
-          },
-          950: {
-            perPage: 2,
-          },
-          680: {
-            perPage: 1,
-          },
-          500: {
-            perPage: 1,
-            padding: {
-              left : '1rem',
-              right:0,
-          },
-          },
-        }
-    }).mount();
+      },
+    },
+  }).mount();
 }
 
 // MODAL IMG
-modalIMG.forEach(item => {
-    item.addEventListener("click", event => {
-       modalEle.style.display = "block";
-       modalImage.src = event.target.src;
-       document.body.style.overflow = "hidden";
-    });
+modalIMG.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    modalEle.style.display = 'block';
+    modalImage.src = event.target.src;
+    document.body.style.overflow = 'hidden';
   });
-  window.onclick = function(event) {
-   if (event.target === modalEle) {
-     modalEle.style.display = "none";
-     document.body.style.overflow = "visible";
-   }
+});
+window.onclick = function (event) {
+  if (event.target === modalEle) {
+    modalEle.style.display = 'none';
+    document.body.style.overflow = 'visible';
   }
-  close.addEventListener("click", () => {
-    modalEle.style.display = "none";
-    document.body.style.overflow = "visible";
-  });
+};
+close.addEventListener('click', () => {
+  modalEle.style.display = 'none';
+  document.body.style.overflow = 'visible';
+});
